04ffbcbf7041a146287150f5f949aff96c9fba50
Ankur <annkur020@gmail.com>
2024-03-29 09:50:17 +0530

    useSearchParam

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
+"use client";
 import React from "react";
 import NavbarItem from "./NavbarItem";
+import { Suspense } from "react";
 
 const Navbar = () => {
   return (
     <div className="flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-4">
-      <NavbarItem title="Trending" param="fetchTrending" />
-      <NavbarItem title="Top Rated" param="fetchTopRated" />
+      <Suspense>
+        <NavbarItem title="Trending" param="fetchTrending" />
+        <NavbarItem title="Top Rated" param="fetchTopRated" />
+      </Suspense>
     </div>
   );
 };
